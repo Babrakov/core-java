766e17f92f0ab6b5a7bb2a96c3ff925446ccdfdb
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-20 21:38:27 +0300

    Added list fonts app.

---
+++ src/listFonts/ListFonts.java
@@ -0,0 +1,15 @@
+package listFonts;
+
+import java.awt.*;
+
+public class ListFonts
+{
+   public static void main(String[] args)
+   {
+      String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment()
+            .getAvailableFontFamilyNames();
+
+      for (String fontName : fontNames)
+         System.out.println(fontName);
+   }
+}
