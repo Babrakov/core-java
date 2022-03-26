6ff022d501ce1872ff74ec23be292de321d2e444
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-26 16:07:47 +0300

    Frames's borders.

---
+++ src/swing/s13border/BorderTest.java
@@ -0,0 +1,17 @@
+package swing.s13border;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class BorderTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         var frame = new BorderFrame();
+         frame.setTitle("BorderTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
