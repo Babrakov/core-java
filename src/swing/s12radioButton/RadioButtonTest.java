c3bb15d4af35a6d8308d6c104e281ce46395d9cc
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-26 13:01:26 +0300

    Radio button.

---
+++ src/swing/s12radioButton/RadioButtonTest.java
@@ -0,0 +1,19 @@
+package swing.s12radioButton;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class RadioButtonTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         var frame = new RadioButtonFrame();
+         frame.setTitle("RadioButtonTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         // set JFrame in center of the screen
+         frame.setLocationRelativeTo(null);
+         frame.setVisible(true);
+      });
+   }
+}
