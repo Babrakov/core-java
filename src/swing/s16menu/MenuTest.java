d8756c39a1c883391099edc5cc63de5c350e2415
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-27 15:28:58 +0300

    Added menu panel with various items.

---
+++ src/swing/s16menu/MenuTest.java
@@ -0,0 +1,17 @@
+package swing.s16menu;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MenuTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         var frame = new MenuFrame();
+         frame.setTitle("MenuTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
