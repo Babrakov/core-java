ffafc3cbe0e09e9f43fc46f7a09f6c4a1e924bfb
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-28 22:14:23 +0300

    Toolbars.

---
+++ src/swing/s17toolbar/ToolBarTest.java
@@ -0,0 +1,17 @@
+package swing.s17toolbar;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ToolBarTest
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         var frame = new ToolBarFrame();
+         frame.setTitle("ToolBarTest");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
