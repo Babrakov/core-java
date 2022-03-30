91075dcc1e6391018f91f38d6587495e3695b48d
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-03-30 22:58:56 +0300

    Finalize swing.

---
+++ src/swing/s19calculator/Calculator.java
@@ -0,0 +1,17 @@
+package swing.s19calculator;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Calculator
+{
+   public static void main(String[] args)
+   {
+      EventQueue.invokeLater(() -> {
+         var frame = new CalculatorFrame();
+         frame.setTitle("Calculator");
+         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         frame.setVisible(true);
+      });
+   }
+}
