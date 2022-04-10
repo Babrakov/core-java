7b2a4c1068f6de1df02e976a27aa2f702ec603d0
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-10 12:36:46 +0300

    Tested process and process builder.

---
+++ src/ch13javafx/p01notHelloWorld/NotHelloWorld.java
@@ -0,0 +1,30 @@
+package ch13javafx.p01notHelloWorld;
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.*;
+
+public class NotHelloWorld extends Application
+{
+   private static final int MESSAGE_X = 75;
+   private static final int MESSAGE_Y = 100;
+   
+   private static final int PREFERRED_WIDTH = 300;
+   private static final int PREFERRED_HEIGHT = 200;
+
+   public void start(Stage stage)
+   {
+      Text message = new Text(MESSAGE_X, MESSAGE_Y,
+         "Not a Hello World program");
+      
+      Pane root = new Pane(message);
+      root.setPrefSize(PREFERRED_WIDTH, PREFERRED_HEIGHT);
+      
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.setTitle("NotHelloWorld");
+      stage.show();
+   }
+}
