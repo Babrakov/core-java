22576c5463db16899f930f773b2823095c72e20b
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-04-19 22:22:56 +0300

    Added Java FX apps.

---
+++ src/ch13javafx/p13binding/BindingDemo3.java
@@ -0,0 +1,48 @@
+package ch13javafx.p13binding;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import static javafx.beans.binding.Bindings.createBooleanBinding;
+import static javafx.beans.binding.Bindings.lessThanOrEqual;
+
+public class BindingDemo3 extends Application
+{
+   public void start(Stage stage)
+   {      
+      Button smaller = new Button("Smaller");
+      Button larger = new Button("Larger");
+      Rectangle gauge = new Rectangle(0, 5, 50, 15);
+      Rectangle outline = new Rectangle(0, 5, 100, 15);
+      outline.setFill(null);
+      outline.setStroke(Color.BLACK);
+      Pane pane = new Pane(gauge, outline);
+      
+      smaller.setOnAction(
+         event -> gauge.setWidth(gauge.getWidth() - 10));
+      larger.setOnAction(
+         event -> gauge.setWidth(gauge.getWidth() + 10));
+
+      // Using Bindings operator
+
+      smaller.disableProperty().bind(
+         lessThanOrEqual(gauge.widthProperty(), 0));      
+      
+      // Creating a binding from a lambda
+      
+      larger.disableProperty().bind(
+         createBooleanBinding(
+            () -> gauge.getWidth() >= 100, // This lambda is computed ...
+            gauge.widthProperty())); // ... when this property changes
+
+      Scene scene = new Scene(new HBox(10, smaller, pane, larger));
+      stage.setScene(scene);
+      stage.show();
+   }
+}
